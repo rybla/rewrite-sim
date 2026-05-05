97d82f056f15986e449ed61b242205fd5c722a4e
Henry Blanchette <henry@monkeyface.com>
2026-05-05 11:49:17 -0400

    [checkpoint]

--- src/RewriteSim/Example/Drv.purs
+++ src/RewriteSim/Example/Drv.purs
@@ -4,7 +4,7 @@ import Prelude
 
 import Control.Monad.Error.Class (class MonadError, throwError)
 import Control.Monad.Reader (class MonadReader, ask, local)
-import Control.Monad.State (class MonadState, gets, modify_)
+import Control.Monad.State (class MonadState, gets)
 import Data.Array as Array
 import Data.Foldable (intercalate, length, traverse_)
 import Data.Lens (view, (%~), (.=))
@@ -15,15 +15,19 @@ import Data.List as List
 import Data.Map (Map)
 import Data.Maybe (Maybe(..))
 import Data.Tuple (uncurry)
-import Data.Tuple.Nested ((/\))
-import RewriteSim (GenericExpr(..), MetaVar(..), asExpr)
-import RewriteSim.Example.Common (Expr, ExprLabel, AbsExpr)
+import RewriteSim (GenericExpr(..), MetaVar)
+import RewriteSim.Example.Common (AbsExpr, ExprLabel)
 import Type.Proxy (Proxy(..))
 
+--------------------------------------------------------------------------------
+
+type SequentLabel = ExprLabel
+type Sequent = AbsExpr
+
 type SortSystem s =
-  { kidSortsOfLabel :: ExprLabel -> Array s
-  , sortOfLabel :: ExprLabel -> s
-  , showExpr :: AbsExpr -> String
+  { kidSortsOfLabel :: SequentLabel -> Array s
+  , sortOfLabel :: SequentLabel -> s
+  , showExpr :: Sequent -> String
   }
 
 type SortingState s =
@@ -31,11 +35,11 @@ type SortingState s =
 
 type SortingContext s =
   { sortSystem :: SortSystem s
-  , stack :: List AbsExpr
+  , stack :: List Sequent
   }
 
 type SortingError =
-  { stack :: List AbsExpr
+  { stack :: List Sequent
   , message :: String
   }
 
@@ -52,7 +56,7 @@ throwSortingError message = do
     , message
     }
 
-checkSort
+checkSortOfSequent
   :: forall m s
    . Eq s
   => Show s
@@ -60,40 +64,40 @@ checkSort
   => MonadState (SortingState s) m
   => MonadError SortingError m
   => s
-  -> AbsExpr
+  -> Sequent
   -> m Unit
-checkSort expectedSort e =
-  local (prop (Proxy @"stack") %~ List.Cons e) do
+checkSortOfSequent expectedSort sequent =
+  local (prop (Proxy @"stack") %~ List.Cons sequent) do
     ctx <- ask
-    case e of
+    case sequent of
       MetaExpr x -> do
         gets (view (prop (Proxy @"metaVarSorts") <<< at x)) >>= case _ of
           Nothing -> do
             prop (Proxy @"metaVarSorts") <<< at x .= Just expectedSort
           Just actualSort -> do
             unless (actualSort == expectedSort) do
-              throwSortingError $ "The meta variable " <> show e <> " is expected to have sort " <> show expectedSort <> " but it actually has sort " <> show actualSort <> " as inferred from its other appearances."
+              throwSortingError $ "The meta variable " <> show sequent <> " is expected to have sort " <> show expectedSort <> " but it actually has sort " <> show actualSort <> " as inferred from its other appearances."
       Expr l kids -> do
         let actualSort = ctx.sortSystem.sortOfLabel l
         unless (actualSort == expectedSort) do
-          throwSortingError $ "The expression " <> ctx.sortSystem.showExpr e <> " is expected to have sort " <> show expectedSort <> " but it actually has sort " <> show actualSort <> "."
+          throwSortingError $ "The sequent " <> ctx.sortSystem.showExpr sequent <> " is expected to have sort " <> show expectedSort <> " but it actually has sort " <> show actualSort <> "."
         let kidSorts = ctx.sortSystem.kidSortsOfLabel l
         unless (length kids == (length kidSorts :: Int)) do
           throwSortingError $ "A sort with label " <> show l <> " is expected to have " <> show (length kidSorts :: Int) <> " kids of sorts " <> (kidSorts # map show # intercalate ", " # \s' -> "[" <> s' <> "]") <> " but it actually has " <> show (length kids :: Int) <> " kids."
-        Array.zip kidSorts kids # traverse_ (uncurry checkSort)
+        Array.zip kidSorts kids # traverse_ (uncurry checkSortOfSequent)
 
-type Rule =
-  { label :: String
-  , hypotheses :: Array AbsExpr
-  , conclusion :: AbsExpr
+--------------------------------------------------------------------------------
+
+type DerivationLabel = ExprLabel
+type Derivation = AbsExpr
+
+type DerivationRule =
+  { label :: ExprLabel
+  , hypotheses :: Array Sequent
+  , conclusion :: Sequent
   }
 
-type RuleSystem = {}
+type DerivationSystem =
+  { rules :: Array DerivationRule
+  }
 
--- type Rule = {
-
--- }
-
--- checkDerivation :: Expr -> 
-
--- checkExpr ::  Expr -> m Expr
